bbe7d959afc687e67133f0a67e5d2956811f1a42
刘荣 <liurong@mastercom.cn>
2023-01-09 15:47:37 +0800

    二级树组件优化

--- src/components/multi-select-tree/select-tree2.vue
+++ src/components/multi-select-tree/select-tree2.vue
@@ -1,10 +1,25 @@
 <!-- 多选树组件 -->
 <template>
   <div class="select-tree">
-    <el-select v-model="selectList" multiple clearable collapse-tags size="small" placeholder="请选择" @clear="reset">
-      <el-option value="" style="display: none;" />
-      <el-tree :data="options" :props="defaultProps" default-expand-all node-key="label" show-checkbox
-        @check="handleChecked" ref="tree" />
+    <el-select
+      v-model="selectList"
+      multiple
+      clearable
+      collapse-tags
+      size="small"
+      placeholder="请选择"
+      @clear="reset"
+    >
+      <el-option value="" style="display: none" />
+      <el-tree
+        :data="options"
+        :props="defaultProps"
+        default-expand-all
+        node-key="label"
+        show-checkbox
+        @check="handleChecked"
+        ref="tree"
+      />
     </el-select>
   </div>
 </template>
@@ -34,70 +49,60 @@ export default {
         label: 'label',
       },
       selectList: [],
-      isManualSelect: false,
+      isNeedSetNodes: true, // 是否需要重新设置节点树
     };
   },
   computed: {
     refTree() {
       return this.$refs.tree;
-    }
+    },
   },
   watch: {
     value: {
       handler(newVal) {
         this.selectList = newVal;
       },
-    }
+    },
   },
   mounted() {
-    // 外界传值
-    this.$watch('selectList', (newList) => {
-      this.$emit('change', newList);
-      if (this.isManualSelect) {
-        this.isManualSelect = false;
-        return;
-      }
-      this.$nextTick(() => {
-        if (Array.isArray(newList)) {
-          this.refTree?.setCheckedKeys(newList);
+    this.$watch(
+      'selectList',
+      (newList) => {
+        this.$emit('change', newList);
+        if (!this.isNeedSetNodes) {
+          this.isNeedSetNodes = true;
+          return;
         }
-      })
-    }, { immediate: true })
+        this.$nextTick(() => {
+          this.refTree?.setCheckedKeys(newList);
+        });
+      },
+      { immediate: true }
+    );
   },
   methods: {
     reset() {
       this.$emit('change', []);
     },
     handleChecked(curObj) {
-      const curCheckedKeys = this.flatNodeKeys([curObj]);
+      const curCheckedKey = curObj.label;
       const checkedKeys = this.refTree.getCheckedKeys(true);
-      // 选中状态
+      
       if (checkedKeys.length > this.selectList.length) {
-        if (curCheckedKeys.includes('全部')) {
-          return this.selectList = ['全部'];
+        if (curCheckedKey === '全部') {
+          return (this.selectList = ['全部']);
         } else if (checkedKeys.includes('全部')) {
-          return this.selectList = checkedKeys.filter(key => key !== '全部');
-        }
-      }  
-      // 取消状态
-      this.selectList = checkedKeys;
-      this.isManualSelect = true;
-    },
-    flatNodeKeys(tarData) {
-      const res = [];
-      for (const node of tarData) {
-        if (node.children) {
-          res.push(...this.flatNodeKeys(node.children));
-        } else {
-          res.push(node.label);
+          return (this.selectList = checkedKeys.filter((key) => key !== '全部'));
         }
       }
-      return res;
-    }
+      this.selectList = checkedKeys;
+      this.isNeedSetNodes = false;
+    },
   },
 };
 </script>
 
 <style lang="less" scoped>
-.select-tree {}
+.select-tree {
+}
 </style>
